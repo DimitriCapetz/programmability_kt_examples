a71485ef25f32db718b9af16a35e9f6f097c3c34
Dimitri Capetz <dimitri.capetz@gmail.com>
2019-08-05 15:15:37 -0500

    Added multiple command logic

--- eapi.py
+++ eapi.py
@@ -9,10 +9,16 @@ from pprint import pprint
 
 switch = Server("https://admin:arista@127.0.0.1:23443/command-api")
 
-response = switch.runCmds(1, ["show version"])
+response = switch.runCmds(1, ["show version", "show hostname"])
 
 pprint(response)
 
 #system_mac = response[0]["systemMacAddress"]
+#hostname = response[1]["hostname"]
+#int_eth1 = response[3]["cmds"]["interface Ethernet1"]["cmds"]
 
 #print system_mac
+#print hostname
+#print "Config Items for interface Ethernet1"
+#for cmd in int_eth1:
+#    print "   " + cmd
