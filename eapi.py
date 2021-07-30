f92dbe0325b7a5f9d7d5c7b719d9bb1ac3944095
Dimitri Capetz <dimitri.capetz@gmail.com>
2021-07-30 19:42:21 +0000

    Updated examples

--- eapi.py
+++ eapi.py
@@ -7,7 +7,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 from jsonrpclib import Server
 from pprint import pprint
 
-switch = Server("https://admin:arista@127.0.0.1:23443/command-api")
+switch = Server("https://eapi-user:eapi-password@10.100.100.1/command-api")
 
 response = switch.runCmds(1, ["show version", "show hostname"])
 
@@ -21,3 +21,20 @@ pprint(response)
 #print "Config Items for interface Ethernet1"
 #for cmd in int_eth1:
 #    print "   " + cmd
+
+
+# FOR REFERENCE
+# MY LOCAL MAC
+# a483.e715.ee67
+
+#mac = input('Which MAC would you like to find? ')
+#
+#mac_table = switch.runCmds(1, ["show mac address-table address {mac}".format(mac = mac)])
+#pprint(mac_table)
+
+#interface = mac_table[0]['unicastTable']['tableEntries'][0]['interface']
+#print("MAC Address {mac} is located on interface {interface}".format(
+#    mac = mac,
+#    interface = interface
+#))
+
